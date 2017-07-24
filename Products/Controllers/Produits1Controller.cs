c90634723598711aed23fa4cf49fda1ca217b9a3
OumeimaZ <oumeima.zarai@esprit.tn>
2017-07-24 10:13:52 +0200

    Ajoutez des fichiers projet.

---
+++ Products/Controllers/Produits1Controller.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Products.Models;
+
+namespace Products.Controllers
+{
+    public class Produits1Controller : ApiController
+    {
+        private ProductsContext db = new ProductsContext();
+
+        // GET: api/Produits1
+        public IQueryable<Produit> GetProduits()
+        {
+            return db.Produits;
+        }
+
+        // GET: api/Produits1/5
+        [ResponseType(typeof(Produit))]
+        public IHttpActionResult GetProduit(int id)
+        {
+            Produit produit = db.Produits.Find(id);
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(produit);
+        }
+
+        // PUT: api/Produits1/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProduit(int id, Produit produit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != produit.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(produit).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProduitExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Produits1
+        [ResponseType(typeof(Produit))]
+        public IHttpActionResult PostProduit(Produit produit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Produits.Add(produit);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = produit.Id }, produit);
+        }
+
+        // DELETE: api/Produits1/5
+        [ResponseType(typeof(Produit))]
+        public IHttpActionResult DeleteProduit(int id)
+        {
+            Produit produit = db.Produits.Find(id);
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            db.Produits.Remove(produit);
+            db.SaveChanges();
+
+            return Ok(produit);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProduitExists(int id)
+        {
+            return db.Produits.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Products/Models/Produit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Products.Models
+{
+    public class Produit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Details { get; set; }
+    }
+}
