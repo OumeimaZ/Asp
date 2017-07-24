e2e1daf76d9af230f6fe792b2c20856d12b4ebed
OumeimaZ <oumeima.zarai@esprit.tn>
2017-07-24 10:15:51 +0200

    Asp.net(IntilaQ)

--- Products/Models/Produit.cs
+++ Products/Models/Produit.cs
@@ -10,6 +10,7 @@ namespace Products.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+
         public string Details { get; set; }
     }
 }
